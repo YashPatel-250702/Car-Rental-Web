28f9cc44c81df022e761c36e77ae890ca722cf60
Yash-123patel <patelyash250702@gmail.com>
2025-03-17 15:53:51 +0530

    Created Booking Entity and reloved some error

--- src/main/java/com/tekworks/rental/entity/BookingHistory.java
+++ src/main/java/com/tekworks/rental/entity/BookingHistory.java
@@ -1,7 +1,41 @@
 package com.tekworks.rental.entity;
 
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingHistory {
 
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bookingCity;
+    private String pickupLocation;
+    private String dropoffLocation;
+    private LocalDateTime bookingDate;
+    private LocalDateTime pickupDate;
+    private LocalDateTime dropoffDate;
+    private Double totalCost;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private Users user; 
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "id")
+    private Cars car;
 }
--- src/main/java/com/tekworks/rental/service/CarService.java
+++ src/main/java/com/tekworks/rental/service/CarService.java
@@ -98,8 +98,8 @@ public class CarService {
     }
 
 
-}
+
 
 }
-}
+
 
